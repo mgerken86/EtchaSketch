d6cc54959c56d159fa0151c3f02a196579f23056
Mark Gerken <mgerken86@gmail.com>
2022-04-02 09:19:20 -0600

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const containerDiv = document.querySelector("#container");
-let gridCount = 0;
 
-while (gridCount < 32) {
-    const gridDivs = document.createElement("div");
-    gridDivs.classList.add("content");
-    gridDivs.textContent = "hello";
-    containerDiv.appendChild(gridDivs);
-    gridCount++;
-    console.log(gridCount);
+function makeRows(rows, columns){
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let square = document.createElement("div");
+        containerDiv.appendChild(square).className = "grid-item";
+    }
 }
+
+makeRows(16, 16);
